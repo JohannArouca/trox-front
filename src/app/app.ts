9dc042043fb29383b418c882c6e0b3187996fc35
Johann Arouca <aroucajohann@yahoo.com>
2025-09-07 13:01:26 -0300

    criacao da secao sobre

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { Menu } from './components/menu/menu';
 import { Inicio } from './components/inicio/inicio';
+import { Sobre } from './components/sobre/sobre';
 
 @Component({
   selector: 'app-root',
-  imports: [Menu, Inicio],
+  imports: [Menu, Inicio, Sobre],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
